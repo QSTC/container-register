bb15fdaf372d309e386376df3e685a97f400f957
Quentin Sustrac <qsustrac@gmail.com>
2021-08-19 16:50:29 +0200

    edit of fun.py for export

--- fun.py
+++ fun.py
@@ -1,6 +1,12 @@
+import pandas as pd
 
 
 dico={'keys':['Ponpon','Squalou','Yoti'],
        'values':['https://www.youtube.com/watch?v=I2PC8FjmIuo',
                  'https://www.youtube.com/watch?v=d7TVXZx3JNo',
                  'https://www.youtube.com/watch?v=H686vAjvVDU']}
+
+
+def show_link():
+
+    pass
