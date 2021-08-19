788f07954d214c3e03c399e838c531736f88fa3e
Quentin Sustrac <qsustrac@gmail.com>
2021-08-19 16:47:01 +0200

    new fun function preparation

---
+++ fun.py
@@ -0,0 +1,6 @@
+
+
+dico={'keys':['Ponpon','Squalou','Yoti'],
+       'values':['https://www.youtube.com/watch?v=I2PC8FjmIuo',
+                 'https://www.youtube.com/watch?v=d7TVXZx3JNo',
+                 'https://www.youtube.com/watch?v=H686vAjvVDU']}
